6bc6fe066e6d055e237e791615967e34a2e36c55
Materioprima <mateoburbano03@gmail.com>
2019-03-31 17:28:34 +0200

    Añadiendo mas clases

---
+++ src/Clases/NinosJovenes.java
@@ -0,0 +1,73 @@
+package Clases;
+
+/**
+ * @author nico-
+ */
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class NinosJovenes implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long codigo;
+    @Column(length=30,nullable = false)
+    private String nombre,apellidos;
+    private String estado,sexo,grado;
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento,fechaEntrada,fechaAltaProyecto,fechaSalidaProyecto,fechaAlta,fechaSalidaAcoes;
+    private double foto;
+    @Column(length = 500)
+    private String observaciones;
+
+    public Long getCodigo() 
+    {
+        return codigo;
+    }
+
+    public void setCodigo(Long id) 
+    {
+        this.codigo = id;
+    }
+
+    @Override
+    public int hashCode() 
+    {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) 
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NinosJovenes)) 
+        {
+            return false;
+        }
+        NinosJovenes other = (NinosJovenes) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "practica3.Autor[ id=" + codigo + " ]";
+    }
+    
+}
