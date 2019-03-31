7dbe8aa39a466e06aa22ec5bf921802df18357e2
Nicojc <nicjilcan@uma.es>
2019-03-31 18:31:10 +0200

    Update SaldoGlobal.java

--- src/Clases/SaldoGlobal.java
+++ src/Clases/SaldoGlobal.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 public class SaldoGlobal implements Serializable 
 {
     private static final long serialVersionUID = 1L;
