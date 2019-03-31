b98ceff2122c920fd761b6f7c1e4e2994eedb3f1
Torse <Torse1996@uma.es>
2019-03-31 18:27:18 +0200

    Anadiendo clases

---
+++ src/Clases/OrdenPago.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class OrdenPago implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int num_orden;
+    @Column(length=30,nullable = false)
+    private String emitida_por;
+    @Temporal(TemporalType.DATE)
+    private Date fecha_emi;
+
+    public int getNum_Orden() 
+    {
+        return num_orden;
+    }
+
+    public void setNum_Orden(int id) 
+    {
+        this.num_orden = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return num_orden;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrdenPago other = (OrdenPago) obj;
+        if (this.num_orden != other.num_orden) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "practica3.OrdenPago[ Número de orden =" + num_orden + " ]";
+    }
+    
+}
---
+++ src/Clases/SaldoGlobal.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class SaldoGlobal implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(length = 500)
+    private String observaciones;
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public void setNum_Orden(int id) 
+    {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SaldoGlobal other = (SaldoGlobal) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "practica3.SaldoGlobal[ ID = " + id + "Fecha = " + fecha +"]";
+    }
+    
+}
