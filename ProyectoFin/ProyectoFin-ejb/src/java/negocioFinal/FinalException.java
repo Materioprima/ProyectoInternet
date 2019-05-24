226b0d6fdcee8f58a76ffe322c9a68992f2bd8b5
Materioprima <mateoburbano03@gmail.com>
2019-05-24 17:24:37 +0200

    Shinsekai

---
+++ ProyectoFin/ProyectoFin-ejb/src/java/negocioFinal/FinalException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocioFinal;
+
+/**
+ *
+ * @author Materio
+ */
+public class FinalException extends Exception {
+
+    public FinalException() {
+    }
+
+    public FinalException(String msg) {
+        super(msg);
+    }
+}
---
+++ ProyectoFin/ProyectoFin-ejb/src/java/negocioFinal/Negocio.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocioFinal;
+
+/* import es.uma.informatica.sii.agendaee.entidades.Contacto; */
+import entidadesFinal.Academico;
+import entidadesFinal.Usuario;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Materio
+ */
+@Local
+public interface Negocio {
+    public void registrarUsuario(Usuario u)throws FinalException;
+    public void validarCuenta(String cuenta, String validacion) throws FinalException;
+    public void compruebaLogin(Usuario u) throws FinalException;
+    public Usuario refrescarUsuario(Usuario u) throws FinalException;
+    public List<Academico> mostrarAcademico();
+    public void eliminarAcademico(Academico a)throws FinalException;
+    public void insertarAcademico(Academico a)throws FinalException;
+    public void modificarAcademico(Academico a)throws FinalException;
+    /*public void modificar(Contacto c) throws AgendaException;
+    public void insertar(Contacto c)throws AgendaException;
+    public void eliminarContacto(Contacto c)throws AgendaException;*/
+}
+
